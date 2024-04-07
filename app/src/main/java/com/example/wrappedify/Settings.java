c7a19a698ac06e327240aa184e0f89427af637be
hjjani <hjmusician@gmail.com>
2024-04-07 14:51:37 -0400

    implemented change/reset password via email

--- app/src/main/java/com/example/wrappedify/Settings.java
+++ app/src/main/java/com/example/wrappedify/Settings.java
@@ -1,6 +1,11 @@
 package com.example.wrappedify;
 
+import static java.security.AccessController.getContext;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -8,22 +13,44 @@ import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.wrappedify.settingsPage.deleteAccount;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 public class Settings extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     FirebaseUser user;
+    ActivityResultLauncher<Intent> imagePickLauncher;
+    Uri selectedImageUri;
+    ImageView profilePic;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        imagePickLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null && data.getData() != null) {
+                            selectedImageUri = data.getData();
+                            setProfilePic(this, selectedImageUri, profilePic);
+                        }
+                    }
+                });
 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
@@ -33,6 +60,7 @@ public class Settings extends AppCompatActivity {
         SwitchCompat nightToggle = (SwitchCompat) findViewById(R.id.nightModeSwitch);
         ImageView backBtn = (ImageView) findViewById(R.id.backBtn);
         RelativeLayout securityAndPrivacy = findViewById(R.id.securityAndPrivacy);
+        profilePic = findViewById(R.id.profile_pic);
 
         username.setText(user.getEmail());
 
@@ -48,7 +76,19 @@ public class Settings extends AppCompatActivity {
             finish();
         });
 
+        profilePic.setOnClickListener((v) -> {
+            ImagePicker.with(this).cropSquare().compress(512).maxResultSize(512, 512)
+                    .createIntent(new Function1<Intent, Unit>() {
+                        @Override
+                        public Unit invoke(Intent intent) {
+                            imagePickLauncher.launch(intent);
+                            return null;
+                        }
+                    });
+        });
+
     }
+
 
     public void goEditProfile() {
         return;
@@ -60,6 +100,10 @@ public class Settings extends AppCompatActivity {
     public void goEditAccount() {
         Intent intent = new Intent(getApplicationContext(), deleteAccount.class);
         startActivity(intent);
+    }
+
+    public void setProfilePic(Context context, Uri imageUri, ImageView imageView) {
+        Glide.with(context).load(imageUri).apply(RequestOptions.circleCropTransform()).into(imageView);
     }
 
 
